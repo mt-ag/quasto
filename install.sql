731cf63b22c3fae12e6b357a9f2105a717ff50c1
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2022-05-06 16:55:27 +0200

    Added install file

---
+++ install.sql
@@ -0,0 +1,2 @@
+PROMPT SQL file to install the QA Tool on the database. Runs the install_qa_tool.sql file.
+@src/install_qa_tool.sql  
---
+++ src/install_qa_tool.sql
@@ -0,0 +1,24 @@
+PROMPT SQL file for installing the QA Tool on a database. Called from install.sql.
+@src/ddl/tab/utplsql_test_case.sql
+@src/ddl/tab/utplsql_test_run.sql
+@src/ddl/tab/utplsql_test_suite.sql
+@src/ddl/tab/qa_rules.sql
+@src/ddl/seq/case_seq.sql
+@src/ddl/seq/run_seq.sql
+@src/ddl/seq/suite_seq.sql
+@src/ddl/cons/utplsql_test_case.sql
+@src/ddl/cons/utplsql_test_run.sql
+@src/ddl/cons/utplsql_test_suite.sql
+@src/ddl/ind/utplsql_test_case.sql
+@src/ddl/ind/utplsql_test_run.sql
+@src/ddl/ind/utplsql_test_suite.sql
+@src/ddl/fk/utplsql_test_case.sql
+@src/ddl/fk/utplsql_test_suite.sql
+@src/plsql/typ/t_varchar2_tab.sql
+@src/plsql/typ/qa_rule_t.sql
+@src/plsql/typ/qa_rules_t.sql
+@src/plsql/pkg/prepare_test_results_pkg.sql
+@src/plsql/pkg/qa_main_pkg.sql
+@src/plsql/trg/case_iu_trg.sql
+@src/plsql/trg/run_iu_trg.sql
+@src/plsql/trg/suite_ui_trg.sql
