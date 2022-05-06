bf3b7d802e6421d73ebb0088a0bf6aaebdc62196
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2022-05-06 16:30:35 +0200

    Add utplsql tables and other db object files to repo

---
+++ src/plsql/trg/case_iu_trg.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE TRIGGER CASE_IU_TRG
+  BEFORE INSERT OR UPDATE ON utplsql_test_case
+  REFERENCING NEW AS NEW OLD AS OLD
+  FOR EACH ROW
+DECLARE
+  l_user VARCHAR2(255);
+BEGIN
+  SELECT nvl(v('APP_USER'), user)
+    INTO l_user
+    FROM dual;
+  IF inserting AND :new.case_id IS NULL THEN
+    :new.case_id := case_seq.nextval;
+  END IF;
+  IF inserting THEN
+    :new.case_create_date := SYSDATE;
+    :new.case_create_user := l_user;
+  END IF;
+  IF updating OR inserting THEN
+    :new.case_change_date := SYSDATE;
+    :new.case_change_user := l_user;
+  END IF;
+END case_ui_trg;
+/
+
+ALTER TRIGGER CASE_IU_TRG ENABLE;
---
+++ src/plsql/trg/suite_ui_trg.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE TRIGGER SUITE_IU_TRG
+  BEFORE INSERT OR UPDATE ON utplsql_test_suite
+  REFERENCING NEW AS NEW OLD AS OLD
+  FOR EACH ROW
+DECLARE
+  l_user VARCHAR2(255);
+BEGIN
+  SELECT nvl(v('APP_USER'), user)
+    INTO l_user
+    FROM dual;
+  IF inserting AND :new.suite_id IS NULL THEN
+    :new.suite_id := suite_seq.nextval;
+  END IF; 
+  IF inserting THEN
+    :new.suite_create_date := SYSDATE;
+    :new.suite_create_user := l_user;
+  END IF;
+  IF updating OR inserting THEN
+    :new.suite_change_date := SYSDATE;
+    :new.suite_change_user := l_user;
+  END IF;
+END suite_ui_trg;
+/
+
+ALTER TRIGGER SUITE_IU_TRG ENABLE;
