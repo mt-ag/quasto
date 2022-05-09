6e41e50ac6914f67d45e73a0bcf3391d8781de9e
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2022-05-09 11:33:08 +0200

    Added table utplsql_calendar and necessary objects

--- src/install_qa_tool.sql
+++ src/install_qa_tool.sql
@@ -1,8 +1,10 @@
 PROMPT SQL file for installing the QA Tool on a database. Called from install.sql.
+@src/ddl/tab/utplsql_calendar.sql
 @src/ddl/tab/utplsql_test_case.sql
 @src/ddl/tab/utplsql_test_run.sql
 @src/ddl/tab/utplsql_test_suite.sql
 @src/ddl/tab/qa_rules.sql
+@src/ddl/seq/cldr_seq.sql
 @src/ddl/seq/case_seq.sql
 @src/ddl/seq/run_seq.sql
 @src/ddl/seq/suite_seq.sql
@@ -19,6 +21,7 @@ PROMPT SQL file for installing the QA Tool on a database. Called from install.sq
 @src/plsql/typ/qa_rules_t.sql
 @src/plsql/pkg/prepare_test_results_pkg.sql
 @src/plsql/pkg/qa_main_pkg.sql
+@src/plsql/trg/cldr_iu_trg.sql
 @src/plsql/trg/case_iu_trg.sql
 @src/plsql/trg/run_iu_trg.sql
 @src/plsql/trg/suite_ui_trg.sql
