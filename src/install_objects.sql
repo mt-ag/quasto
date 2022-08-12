a218acc9cc5395eececfe3eac402110ee43b365e
Oliver Lemm <oliver.lemm@mt-ag.com>
2022-08-12 15:51:39 +0200

    readded utplsql calls

--- src/install_objects.sql
+++ src/install_objects.sql
@@ -1,3 +1,3 @@
 PROMPT SQL file for installing the utPLSQL and QUASTO objects on a database. Called from install.sql.
--- @src/install_utplsql_objects.sql
+@src/install_utplsql_objects.sql
 @src/install_quasto_objects.sql
