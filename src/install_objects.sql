ebedaf89da66a1590a42a5c3dd83bb09a2e73632
PhilippDahlem <philipp.dahlem@mt-ag.com>
2022-08-16 09:24:09 +0200

    Update install_objects.sql

--- src/install_objects.sql
+++ src/install_objects.sql
@@ -1,6 +1,6 @@
 PROMPT SQL file for installing the utPLSQL and QUASTO objects on a database. Called from install.sql.
 -- user_interface to determine if dependet objects should be installed or not
-accept user_input CHAR PROMPT 'Do you have ut_plsql installed? (J/N)' FORMAT 'A20' DEFAULT 'J';
+/*accept user_input CHAR PROMPT 'Do you have ut_plsql installed? (J/N)' FORMAT 'A20' DEFAULT 'J';
 declare 
   v_script_name varchar2 (100 char);
 begin
@@ -11,5 +11,6 @@ begin
   end if;
 end;
 /
+*/
 @src/install_utplsql_objects.sql
 @src/install_quasto_objects.sql
