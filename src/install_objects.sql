759f20b4181e6647dfcd6cfff329ac8da8c008ab
PhilippDahlem <philipp.dahlem@mt-ag.com>
2022-08-16 15:31:02 +0200

    test to fix dialog installation

--- src/install_objects.sql
+++ src/install_objects.sql
@@ -1,16 +1,15 @@
 PROMPT SQL file for installing the utPLSQL and QUASTO objects on a database. Called from install.sql.
+variable script_name varchar2(100 char)
 -- user_interface to determine if dependet objects should be installed or not
-/*accept user_input CHAR PROMPT 'Do you have ut_plsql installed? (J/N)' FORMAT 'A20' DEFAULT 'J';
-declare 
-  v_script_name varchar2 (100 char);
+accept user_input CHAR PROMPT 'Do you have ut_plsql installed? (J/N)' FORMAT 'A20' DEFAULT 'J';
 begin
   if  '&user_Input.' = 'J'
     then
-      v_script_name := 'src/install_utplsql_objects.sql';
-      @&v_script_name;
+    :script_name := 'install_utplsql_objects.sql';
+  else :script_name := 'null.sql'
   end if;
 end;
 /
-*/
-@src/install_utplsql_objects.sql
-@src/install_quasto_objects.sql
+print :script_name
+--@src/:script_name
+--@src/install_quasto_objects.sql
