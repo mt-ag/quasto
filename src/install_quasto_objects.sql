10d5d39f4693e7f4e687895f38e94196798c862a
Oliver Lemm <oliver.lemm@mt-ag.com>
2022-08-17 18:40:55 +0200

    PROMPT added and execute adjusted

--- src/install_quasto_objects.sql
+++ src/install_quasto_objects.sql
@@ -1,15 +1,37 @@
 PROMPT Running SQL file for installing the QUASTO objects on the database. Called from install_objects.sql.
+PROMPT src/ddl/tab/qa_rules.sql 
 @src/ddl/tab/qa_rules.sql
+
+PROMPT src/ddl/ind/qa_rules.sql
 @src/ddl/ind/qa_rules.sql
+
+PROMPT src/ddl/cons/qa_rules.sql
 @src/ddl/cons/qa_rules.sql
-@src/ddl/seq/qaru_seq.sql
+
+PROMPT src/plsql/typ/qa_rule_t.sql
 @src/plsql/typ/qa_rule_t.sql
+
+PROMPT src/plsql/typ/qa_rule_t.sql
+@src/plsql/typ/qa_rule_t.sql
+
+PROMPT src/plsql/typ/qa_rules_t.sql
 @src/plsql/typ/qa_rules_t.sql
+
+PROMPT src/plsql/pkg/qa_main_pkg.sql
 @src/plsql/pkg/qa_main_pkg.sql
+
+PROMPT src/plsql/pkg/qa_api_pkg.sql
 @src/plsql/pkg/qa_api_pkg.sql
+
+PROMPT src/plsql/trg/qaru_iu_trg.sql
 @src/plsql/trg/qaru_iu_trg.sql
+
+PROMPT src/plsql/syn/qa_rule_t.sql
 @src/plsql/syn/qa_rule_t.sql
+
+PROMPT src/plsql/fn/fc_export_qa_rules.sql
 @src/plsql/fn/fc_export_qa_rules.sql
 
-grant execute qa_api_pkg to public;
-grant execute qa_rule_t to public;
+PROMPT Grants
+grant execute on qa_api_pkg to public;
+grant execute on qa_rule_t to public;
