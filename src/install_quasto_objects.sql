57f55cdf7703c6ed80483e5b3893c8860496d649
Oliver Lemm <oliver.lemm@mt-ag.com>
2022-08-17 15:29:07 +0200

    qa_api_pkg added to install and uninstall

--- src/install_quasto_objects.sql
+++ src/install_quasto_objects.sql
@@ -6,5 +6,6 @@ PROMPT Running SQL file for installing the QUASTO objects on the database. Calle
 @src/plsql/typ/qa_rule_t.sql
 @src/plsql/typ/qa_rules_t.sql
 @src/plsql/pkg/qa_main_pkg.sql
+@src/plsql/pkg/qa_api_pkg.sql
 @src/plsql/trg/qaru_iu_trg.sql
 @src/plsql/fn/fc_export_qa_rules.sql
