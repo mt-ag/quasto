5df035cfa7a5205343fc4a925e5f601fb70ef0be
PhilippDahlem <philipp.dahlem@mt-ag.com>
2022-11-16 16:43:23 +0100

    Updated Export Functions

--- src/plsql/pkg/qa_export_import_rules_pkg.sql
+++ src/plsql/pkg/qa_export_import_rules_pkg.sql
@@ -1,8 +1,9 @@
 create or replace package qa_export_import_rules_pkg is
 
   gc_scope constant varchar2(100) := $$plsql_unit || '.';
+  g_version_number number := 1.1;
 
-  -- if spool set to active, all outputs are deactivated 
+  -- if spool set to active, all outputs are deactivated
   -- otherwise you would get extra lines in your JSON
   g_spool_active boolean := false;
 
@@ -11,6 +12,9 @@ create or replace package qa_export_import_rules_pkg is
     pi_client_name in qa_rules.qaru_client_name%type
    ,pi_category    in qa_rules.qaru_category%type default null
   ) return clob;
+
+  -- Wrapper Function to create an executable .sql Script to import Rules
+  function f_export_rules_to_script_clob(pi_clob in clob) return clob;
 
   procedure p_clob_to_output(pi_clob in clob);
 
@@ -103,53 +107,52 @@ create or replace package body qa_export_import_rules_pkg is
                         and qaru_category = category.qaru_category)
           loop
             l_rule_json := json_object_t();
-            l_rule_json.put('qaru_rule_number'
+            l_rule_json.put('rule_number'
                            ,rules.qaru_rule_number);
-            l_rule_json.put('qaru_name'
+            l_rule_json.put('name'
                            ,rules.qaru_name);
-            l_rule_json.put('qaru_object_types'
+            l_rule_json.put('object_types'
                            ,rules.qaru_object_types);
-            l_rule_json.put('qaru_error_message'
+            l_rule_json.put('error_message'
                            ,rules.qaru_error_message);
-            l_rule_json.put('qaru_comment'
+            l_rule_json.put('comment'
                            ,rules.qaru_comment);
-            l_rule_json.put('qaru_exclude_objects'
+            l_rule_json.put('exclude_objects'
                            ,rules.qaru_exclude_objects);
-            l_rule_json.put('qaru_error_level'
+            l_rule_json.put('error_level'
                            ,rules.qaru_error_level);
-            l_rule_json.put('qaru_is_active'
+            l_rule_json.put('is_active'
                            ,rules.qaru_is_active);
             -- currently there is a problem formatting the sql
-            l_rule_json.put('qaru_sql'
+            l_rule_json.put('sql'
                            ,rules.qaru_sql);
-            /*
-            l_rule_json.put('qaru_sql'
-                           ,'');*/
-            l_rule_json.put('qaru_predecessor_ids'
+            l_rule_json.put('predecessor_ids'
                            ,nvl(rules.qaru_predecessor_ids
                                ,''));
-            l_rule_json.put('qaru_layer'
+            l_rule_json.put('layer'
                            ,rules.qaru_layer);
             l_rules_array_json.append(l_rule_json);
           end loop;
         
-          l_category_json.put('qaru_category'
+          l_category_json.put('category'
                              ,category.qaru_category);
           l_category_json.put('rules'
                              ,l_rules_array_json);
           l_category_array_json.append(l_category_json);
         end loop;
       
-        l_client_name_json.put('qaru_client_name'
+        l_client_name_json.put('client_name'
                               ,client.qaru_client_name);
-        l_client_name_json.put('qaru_categories'
+        l_client_name_json.put('categories'
                               ,l_category_array_json);
       end loop;
     
       l_client_name_array_json.append(l_client_name_json);
-      l_client_names_json.put('qaru_client_names'
+      l_client_names_json.put('client_names'
                              ,l_client_name_array_json);
       l_main_array_json.append(l_client_names_json);
+      l_main_json.put('version'
+                     ,g_version_number);
       l_main_json.put('qa_rules'
                      ,l_main_array_json);
     
@@ -157,10 +160,61 @@ create or replace package body qa_export_import_rules_pkg is
       select json_serialize(l_clob returning clob pretty)
       into l_pretty_print
       from dual;
-    
       return l_pretty_print;
     end if;
   end f_export_rules_table_to_clob;
+
+  -- Experimental Function to Export A Json as an executable script
+  function f_export_rules_to_script_clob(pi_clob in clob) return clob is
+    l_clob clob;
+    l_sql  clob;
+  begin
+    l_clob := 'set serveroutput on;' || chr(10);
+    dbms_lob.append(l_clob
+                   ,'declare' || chr(10));
+    dbms_lob.append(l_clob
+                   ,'  l_clob clob;' || chr(10));
+    dbms_lob.append(l_clob
+                   ,'begin' || chr(10));
+    for f in (select line
+              from dual
+                  ,lateral (select regexp_substr(pi_clob
+                                               ,'.+'
+                                               ,1
+                                               ,level
+                                               ,'m') line
+                           from dual
+                           connect by level <= regexp_count(pi_clob
+                                                           ,'.+'
+                                                           ,1
+                                                           ,'m')))
+    loop
+      if dbms_lob.instr(f.line
+                       ,'"sql" : "') > 0 --and 1=0
+      then
+        null;
+        l_sql := replace_with_clob(i_source  => f.line,
+                          i_search  => '\n',
+                          i_replace => 'chr(10)');
+        /*select qaru_sql
+        into l_sql
+        from json_table(f.line
+                       ,'$.qa_rules[*]' columns(nested path '$.client_names[*]' columns(nested path '$.categories[*]' columns(nested path '$.rules[*]' columns(qaru_sql varchar2(400 char) path '$.sql')))));
+      */
+        dbms_lob.append(l_clob
+                       ,'dbms_lob.append(l_clob,' || 'q' || '''' || '[' || l_sql || ']' || ''');' || chr(10));
+      else
+        dbms_lob.append(l_clob
+                       ,'dbms_lob.append(l_clob,' || 'q' || '''' || '[' || f.line || ']' || ''');' || chr(10));
+      end if;
+    end loop;
+    dbms_lob.append(l_clob
+                   ,chr(10) || 'quasto.qa_export_import_rules_pkg.p_import_clob_to_rules_table(pi_clob => l_clob);' || chr(10));
+    dbms_lob.append(l_clob
+                   ,'end;' || chr(10) || '/');
+  
+    return l_clob;
+  end f_export_rules_to_script_clob;
 
 
   procedure p_clob_to_output(pi_clob in clob) is
@@ -182,21 +236,29 @@ create or replace package body qa_export_import_rules_pkg is
   begin
     if pi_clob is not null
     then
+/*      insert into qa_import_files
+        (qaif_id
+        ,qaif_clob_data
+        ,qaif_import_date)
+      values
+        (null
+        ,pi_clob
+        ,trunc(sysdate));*/
       for i in (select *
                 from json_table(pi_clob
-                               ,'$.qa_rules[*]' columns(nested path '$.qaru_client_names[*]' columns(qaru_client_name varchar2(400 char) path '$.qaru_client_name'
-                                               ,nested path '$.qaru_categories[*]' columns(qaru_category varchar2(400 char) path '$.qaru_category'
-                                                       ,nested path '$.rules[*]' columns(qaru_rule_number varchar2(400 char) path '$.qaru_rule_number'
-                                                               ,qaru_name varchar2(400 char) path '$.qaru_name'
-                                                               ,qaru_object_types varchar2(400 char) path '$.qaru_object_types'
-                                                               ,qaru_error_message varchar2(400 char) path '$.qaru_error_message'
-                                                               ,qaru_comment varchar2(400 char) path '$.qaru_comment'
-                                                               ,qaru_exclude_objects varchar2(400 char) path '$.qaru_exclude_objects'
-                                                               ,qaru_error_level varchar2(400 char) path '$.qaru_error_level'
-                                                               ,qaru_is_active varchar2(400 char) path '$.qaru_is_active'
-                                                               ,qaru_sql varchar2(400 char) path '$.qaru_sql'
-                                                               ,qaru_predecessor_ids varchar2(400 char) path '$.qaru_predecessor_ids'
-                                                               ,qaru_layer varchar2(400 char) path '$.qaru_layer'))))))
+                               ,'$.qa_rules[*]' columns(nested path '$.client_names[*]' columns(qaru_client_name varchar2(400 char) path '$.client_name'
+                                               ,nested path '$.categories[*]' columns(qaru_category varchar2(400 char) path '$.category'
+                                                       ,nested path '$.rules[*]' columns(qaru_rule_number varchar2(400 char) path '$.rule_number'
+                                                               ,qaru_name varchar2(400 char) path '$.name'
+                                                               ,qaru_object_types varchar2(400 char) path '$.object_types'
+                                                               ,qaru_error_message varchar2(400 char) path '$.error_message'
+                                                               ,qaru_comment varchar2(400 char) path '$.comment'
+                                                               ,qaru_exclude_objects varchar2(400 char) path '$.exclude_objects'
+                                                               ,qaru_error_level number path '$.error_level'
+                                                               ,qaru_is_active number path '$.is_active'
+                                                               ,qaru_sql varchar2(400 char) path '$.sql'
+                                                               ,qaru_predecessor_ids varchar2(400 char) path '$.predecessor_ids'
+                                                               ,qaru_layer varchar2(400 char) path '$.layer'))))))
       loop
         dbms_output.put_line('MERGE CLIENT_NAME=' || i.qaru_client_name || ' RULE_NUMBER=' || i.qaru_rule_number);
       
