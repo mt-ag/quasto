a6629f29616cff16598d94763d3f294709fb3aba
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2022-11-21 16:54:26 +0100

    #11 - Adjusted logic of utPLSQL generation package to use new rule format

--- src/plsql/pkg/create_ut_test_packages_pkg.sql
+++ src/plsql/pkg/create_ut_test_packages_pkg.sql
@@ -12,7 +12,7 @@ create or replace package body create_ut_test_packages_pkg is
     l_clob         clob;
   begin
     dbms_output.enable(buffer_size => 10000000);
-  
+
     for rec_clients in (select qaru_client_name
                               ,regexp_replace(replace(lower(qaru_client_name)
                                                      ,' '
@@ -23,14 +23,14 @@ create or replace package body create_ut_test_packages_pkg is
                         where qaru_is_active = 1
                         group by qaru_client_name)
     loop
-    
+
       l_package_name := 'ut_qa_test_' || rec_clients.qaru_client_name_unified || '_pkg';
-    
+
       l_clob := 'CREATE OR REPLACE PACKAGE ' || l_package_name || ' IS' || chr(10);
       l_clob := l_clob || '--THIS PACKAGE IS GENERATED AUTOMATICALLY. DO NOT MAKE ANY MANUAL CHANGES.' || chr(10);
       l_clob := l_clob || '--%suite(' || rec_clients.qaru_client_name || ')' || chr(10);
       l_clob := l_clob || '--%suitepath(' || rec_clients.qaru_client_name_unified || ')' || chr(10) || chr(10);
-    
+
       for rec_rules in (select qaru_name
                               ,regexp_replace(replace(lower(qaru_name)
                                                      ,' '
@@ -42,20 +42,20 @@ create or replace package body create_ut_test_packages_pkg is
                         and qaru_is_active = 1
                         order by qaru_id asc)
       loop
-      
+
         l_clob := l_clob || '--%test(is qa rule "' || rec_rules.qaru_name || '" observed)' || chr(10);
         l_clob := l_clob || 'PROCEDURE pr_ut_test_' || rec_rules.qaru_name_unified || ';' || chr(10);
-      
+
       end loop;
-    
+
       l_clob := l_clob || 'END ' || l_package_name || ';';
-    
+
       execute immediate l_clob;
       dbms_output.put_line('Package specification for ' || l_package_name || ' created.');
-    
+
       l_clob := 'CREATE OR REPLACE PACKAGE BODY ' || l_package_name || ' IS' || chr(10);
-    
-      for rec_rules in (select qaru_id
+
+      for rec_rules in (select qaru_rule_number
                               ,replace(qaru_name
                                       ,'' || chr(39) || ''
                                       ,'' || chr(39) || chr(39) || '') as qaru_test_name
@@ -71,7 +71,7 @@ create or replace package body create_ut_test_packages_pkg is
                         and qaru_is_active = 1
                         order by qaru_id asc)
       loop
-      
+
         l_clob := l_clob || 'PROCEDURE pr_ut_test_' || rec_rules.qaru_name_unified || chr(10);
         l_clob := l_clob || 'IS' || chr(10);
         l_clob := l_clob || '  l_app_id NUMBER;' || chr(10);
@@ -81,12 +81,12 @@ create or replace package body create_ut_test_packages_pkg is
         l_clob := l_clob || '  l_object_names CLOB;' || chr(10);
         l_clob := l_clob || '  l_error_message qa_rules.qaru_error_message%type;' || chr(10);
         l_clob := l_clob || 'BEGIN' || chr(10);
-      
+
         if rec_rules.qaru_category = 'APEX'
         then
           if rec_rules.qaru_layer = 'PAGE'
           then
-          
+
             l_clob := l_clob || 'FOR rec IN (SELECT aapp.application_id' || chr(10);
             l_clob := l_clob || '                 , aapp.page_id' || chr(10);
             l_clob := l_clob || '              FROM apex_application_pages aapp' || chr(10);
@@ -95,10 +95,10 @@ create or replace package body create_ut_test_packages_pkg is
             l_clob := l_clob || '  l_app_id := rec.application_id;' || chr(10);
             l_clob := l_clob || '  l_app_page_id := rec.page_id;' || chr(10);
             l_clob := l_clob || '  l_layer := ''' || rec_rules.qaru_layer || ' (Application: ''||rec.application_id||'' - Page: ''||rec.page_id||'')'';' || chr(10);
-          
+
           elsif rec_rules.qaru_layer = 'APPLICATION'
           then
-          
+
             l_clob := l_clob || 'FOR rec IN (SELECT apap.application_id' || chr(10);
             l_clob := l_clob || '              FROM apex_applications apap' || chr(10);
             l_clob := l_clob || '          ORDER BY apap.application_id ASC)' || chr(10);
@@ -106,35 +106,44 @@ create or replace package body create_ut_test_packages_pkg is
             l_clob := l_clob || '  l_app_id := rec.application_id;' || chr(10);
             l_clob := l_clob || '  l_app_page_id := null;' || chr(10);
             l_clob := l_clob || '  l_layer := ''' || rec_rules.qaru_layer || ' (Application: ''||rec.application_id||'')'';' || chr(10);
-          
+
           end if;
-        else
+          
+          l_clob := l_clob || '  qa_main_pkg.p_test_apex_rule(pi_qaru_client_name  => ''' || rec_clients.qaru_client_name || ''',' || chr(10);
+          l_clob := l_clob || '                               pi_qaru_rule_number  => ''' || rec_rules.qaru_rule_number || ''',' || chr(10);
+          l_clob := l_clob || '                               pi_app_id            => l_app_id,' || chr(10);
+          l_clob := l_clob || '                               pi_app_page_id       => l_app_page_id,' || chr(10);
+          l_clob := l_clob || '                               po_result            => l_result,' || chr(10);
+          l_clob := l_clob || '                               po_object_names      => l_object_names,' || chr(10);
+          l_clob := l_clob || '                               po_error_message     => l_error_message);' || chr(10);
         
+        else
+
           l_clob := l_clob || '  l_app_id := null;' || chr(10);
           l_clob := l_clob || '  l_app_page_id := null;' || chr(10);
           l_clob := l_clob || '  l_layer := ''' || rec_rules.qaru_layer || ''';' || chr(10);
-        
+          
+          l_clob := l_clob || '  qa_main_pkg.p_test_rule(pi_qaru_client_name  => ''' || rec_clients.qaru_client_name || ''',' || chr(10);
+          l_clob := l_clob || '                          pi_qaru_rule_number  => ''' || rec_rules.qaru_rule_number || ''',' || chr(10);
+          l_clob := l_clob || '                          po_result            => l_result,' || chr(10);
+          l_clob := l_clob || '                          po_object_names      => l_object_names,' || chr(10);
+          l_clob := l_clob || '                          po_error_message     => l_error_message);' || chr(10);
+
         end if;
-      
-        l_clob := l_clob || '  qa_pkg.test_rule(pi_qaru_id        => ' || to_char(rec_rules.qaru_id) || ',' || chr(10);
-        l_clob := l_clob || '                   pi_app_id         => l_app_id,' || chr(10);
-        l_clob := l_clob || '                   pi_app_page_id    => l_app_page_id,' || chr(10);
-        l_clob := l_clob || '                   po_result         => l_result,' || chr(10);
-        l_clob := l_clob || '                   po_object_names   => l_object_names,' || chr(10);
-        l_clob := l_clob || '                   po_error_message  => l_error_message);' || chr(10);
+
         l_clob := l_clob || '  ut.expect(l_result).to_(equal(1));' || chr(10);
         l_clob := l_clob || '  dbms_output.put_line(''  --INFO-- Test "' || rec_rules.qaru_test_name || '" completed with result ''||l_result);' || chr(10);
         l_clob := l_clob || '  dbms_output.put_line(''           Layer: ''||l_layer);' || chr(10);
         l_clob := l_clob || '  dbms_output.put_line(''           Error message: ''||l_error_message);' || chr(10);
         l_clob := l_clob || '  dbms_output.put_line(''           Invalid objects: ''||l_object_names);' || chr(10);
-      
+
         if rec_rules.qaru_category = 'APEX'
         then
-        
+
           l_clob := l_clob || 'END LOOP;' || chr(10);
-        
+
         end if;
-      
+
         l_clob := l_clob || 'EXCEPTION' || chr(10);
         l_clob := l_clob || '  WHEN OTHERS THEN' || chr(10);
         l_clob := l_clob || '    dbms_output.put_line(''  --ERROR-- Execution of test "' || rec_rules.qaru_test_name || '" raised exception.'');' || chr(10);
@@ -142,13 +151,13 @@ create or replace package body create_ut_test_packages_pkg is
         l_clob := l_clob || '    dbms_output.put_line(''            ''||DBMS_UTILITY.format_error_backtrace);' || chr(10);
         l_clob := l_clob || '    RAISE;' || chr(10);
         l_clob := l_clob || 'END pr_ut_test_' || rec_rules.qaru_name_unified || ';' || chr(10);
-      
+
       end loop;
-    
+
       l_clob := l_clob || 'END ' || l_package_name || ';';
-    
+
     end loop;
-  
+
     execute immediate l_clob;
     dbms_output.put_line('Package body for ' || l_package_name || ' created.');
   exception
