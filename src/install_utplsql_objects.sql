f147afe11c71c8fb410dbc28915db74beee55aa6
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2022-12-15 13:35:16 +0100

    #22: Removed unused custom utPLSQL install files

--- src/install_utplsql_objects.sql
+++ src/install_utplsql_objects.sql
@@ -1,28 +1,5 @@
 PROMPT Running SQL file for installing the utPLSQL objects on the database. Called from install_objects.sql.
-@src/ddl/tab/utplsql_calendar.sql
-@src/ddl/tab/utplsql_test_case.sql
-@src/ddl/tab/utplsql_test_run.sql
-@src/ddl/tab/utplsql_test_suite.sql
-@src/ddl/seq/cldr_seq.sql
-@src/ddl/seq/case_seq.sql
-@src/ddl/seq/run_seq.sql
-@src/ddl/seq/suite_seq.sql
-@src/ddl/ind/utplsql_test_case.sql
-@src/ddl/ind/utplsql_test_run.sql
-@src/ddl/ind/utplsql_test_suite.sql
-@src/ddl/cons/utplsql_calendar.sql
-@src/ddl/cons/utplsql_test_case.sql
-@src/ddl/cons/utplsql_test_run.sql
-@src/ddl/cons/utplsql_test_suite.sql
-@src/ddl/fk/utplsql_test_case.sql
-@src/ddl/fk/utplsql_test_suite.sql
-@src/plsql/typ/varchar2_tab_t.sql
-@src/plsql/pkg/prepare_test_results_pkg.sql
 @src/plsql/pkg/create_ut_test_packages_pkg.sql
-@src/plsql/trg/cldr_iu_trg.sql
-@src/plsql/trg/case_iu_trg.sql
-@src/plsql/trg/run_iu_trg.sql
-@src/plsql/trg/suite_iu_trg.sql
 
 prompt recompile_utplsql_objects.sql 
 @src/scripts/recompile_utplsql_objects.sql
