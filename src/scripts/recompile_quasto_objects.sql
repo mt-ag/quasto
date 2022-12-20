117dbfee30784bf949ce6b2eb044ecacda233807
PhilippDahlem <philipp.dahlem@mt-ag.com>
2022-12-20 14:59:59 +0100

    Simplified the Recompile

--- src/scripts/recompile_quasto_objects.sql
+++ src/scripts/recompile_quasto_objects.sql
@@ -9,7 +9,6 @@ declare
   l_key         varchar2(100 char);
   l_action      varchar2(32767);
   l_action_2    varchar2(32767);
-  l_object_list varchar2(32767);
   l_count       number;
 begin
    -- buffer size extend
@@ -32,7 +31,6 @@ begin
   while l_object is not null
   loop
     l_action_2 := null;
-    l_object_list := l_object_list || l_object_name(l_object) || ',';
     if l_object_type(l_object) = 'PACKAGE'
       then
         l_action   := 'alter package ' || l_object_name(l_object) || ' compile';
@@ -40,63 +38,31 @@ begin
     else
         l_action   := 'alter ' || l_object_type(l_object) || ' ' || l_object_name(l_object) || ' compile';
     end if;
-
-    select count(1)
-    into l_count
-    from user_objects s
-    where object_name = l_object_name(l_object)
-    and object_type = l_object_type(l_object);
   
-    if l_count <> 0
-    then
       execute immediate (l_action);
       if l_action_2 is not null
         then
           execute immediate (l_action_2);
       end if;
-      dbms_output.put_line('INFO: ' || l_object_type(l_object) || ' ' || l_object_name(l_object) || ' recompiled.');
-    else
-      dbms_output.put_line('WARNING: ' || l_object_type(l_object) || ' ' || l_object_name(l_object) || ' does not exist.');
-    end if;
-
-    l_object := l_object_name.next(l_object);
-  end loop;
-    l_object_list :=  '''' || rtrim(l_object_list,',') || '''';
-    --dbms_output.put_line(l_object_list);
     select count(*)
     into l_count
     from user_objects uo
     where uo.status = 'INVALID'
-    and uo.object_name in (  select regexp_substr (
-                             l_object_list,
-                             '[^,]+',
-                             1,
-                             level) value
-                             from   dual
-                             connect by level <= 
-                             length ( l_object_list ) - length ( replace ( l_object_list, ',' ) ) + 1);
-    -- For Debugging purposes
-/*    l_action := q'[select count(*)
-    from user_objects uo
-    where uo.status = 'INVALID'
-    and uo.object_name in (  select regexp_substr (
-    ]' || l_object_list || q'[,
-                             '[^,]+',
-                             1,
-                             level) value
-                             from   dual
-                             connect by level <= 
-                             length ( ]' || l_object_list || q'[) - length ( replace ( ]' || l_object_list || q'[, ',' ) ) + 1)]';
-    dbms_output.put_line(l_action);*/
-    if l_count > 0 
+    and uo.object_name = l_object_name(l_object);
+    
+    if l_count <> 0
       then
-        dbms_output.put_line('ERROR: There are ' || l_count || 'Invalid Quasto objects');
-      else
-        dbms_output.put_line('INFO: There are no invalid Quasto objects');
+        dbms_output.put_line('ERROR: The object ' || l_object_name(l_object) || l_object_type(l_object)  || 'is invalid');
+    else
+        dbms_output.put_line('INFO: ' || l_object_type(l_object) || ' ' || l_object_name(l_object) || ' recompiled.');
     end if;
+
+    l_object := l_object_name.next(l_object);
+  end loop;
     exception
       when others
         then
+          dbms_output.put_line('ERROR: There are ' || l_count || 'Invalid Quasto objects');
           raise;
 end;
 /
