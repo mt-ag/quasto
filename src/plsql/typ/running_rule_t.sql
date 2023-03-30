8225d3134339ee0514f5d86934a612870ae66a35
Sabrina <sabrina.prang@mt-ag.com>
2023-03-30 12:51:24 +0200

    typo correction

--- src/plsql/typ/running_rule_t.sql
+++ src/plsql/typ/running_rule_t.sql
@@ -6,4 +6,4 @@ CREATE OR REPLACE TYPE running_rule_t AS OBJECT
 );
 
 
-CREATE OR REPLACE TYPE running_rules_t IS TABLE OF running_rule;
+CREATE OR REPLACE TYPE running_rules_t IS TABLE OF running_rule_t;
