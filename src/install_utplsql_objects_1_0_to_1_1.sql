6eeb7ec91f7b788c9b0e24d11f5b7a10909c458f
PhilippDahlem <philipp.dahlem@mt-ag.com>
2023-04-06 10:52:28 +0200

    Corrected Installation Process

--- src/install_utplsql_objects_1_0_to_1_1.sql
+++ src/install_utplsql_objects_1_0_to_1_1.sql
@@ -9,3 +9,25 @@ PROMPT PACKAGES
 PROMPT src/plsql/pkg/create_ut_test_packages_pkg.sql
 @src/plsql/pkg/create_ut_test_packages_pkg.sql
 
+PROMPT TABLES
+PROMPT src/ddl/utplsql_calendar_drop.sql
+@src/ddl/utplsql_calendar_drop.sql
+PROMPT src/ddl/utplsql_test_case_drop.sql
+@src/ddl/utplsql_test_case_drop.sql
+PROMPT src/ddl/utplsql_test_suite_drop.sql
+@src/ddl/utplsql_test_suite_drop.sql
+PROMPT src/ddl/utplsql_test_run_drop.sql
+@src/ddl/utplsql_test_run_drop.sql
+
+PROMPT SEQUENCES
+PROMPT src/ddl/cldr_seq_drop.sql
+@src/ddl/cldr_seq_drop.sql
+PROMPT src/ddl/run_seq_drop.sql
+@src/ddl/run_seq_drop.sql
+PROMPT src/ddl/case_seq_drop.sql
+@src/ddl/case_seq_drop.sql
+PROMPT src/ddl/suite_seq_drop.sql
+@src/ddl/suite_seq_drop.sql
+
+PROMPT src/ddl/prepare_test_results_pkg_drop.sql
+@src/ddl/prepare_test_results_pkg_drop.sql
