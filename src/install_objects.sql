a11c1d92e0652bea24c2332d80adeca02b45af64
PhilippDahlem <philipp.dahlem@mt-ag.com>
2023-04-12 09:55:54 +0200

    Corrected Script Name

--- src/install_objects.sql
+++ src/install_objects.sql
@@ -54,6 +54,7 @@ declare
     l_script_name_quasto          varchar2(100) := 'install_quasto_objects.sql';
     l_script_name_quasto_upgrade  varchar2(100) := 'install_quasto_objects_' || replace(:version_old,'.','_') || '_to_' || replace(:version,'.','_') || '.sql';
     l_count                       number;
+    l_count_utplsql               number;
     l_current_version             varchar2 (40 char) := :version;
     l_arg                         number := '~1';
 begin
@@ -65,32 +66,36 @@ begin
     from user_objects
     where object_name = 'QA_MAIN_PKG';
 
-    if 1 = l_arg and l_count = 0
+    -- Check if Pre Version exists
+    select count(1)
+    into l_count_utplsql
+    from user_objects
+    where object_name = 'CREATE_UT_TEST_PACKAGES_PKG';
+    
+    if 1 = l_arg and l_count_utplsql > 0
+      then 
+        l_script_name_utplsql := l_script_name_utplsql_upgrade;
+    elsif 0 = l_arg 
       then
-        l_script_name_utplsql := 'install_utplsql_objects.sql';
-    elsif 1 = l_arg and l_count > 0
+        l_script_name_utplsql := 'null.sql'; 
+    end if;
+
+    if l_count > 0
       then
         if l_current_version != '1.0'
           then
-            l_script_name_utplsql := l_script_name_utplsql_upgrade;
             l_script_name_quasto  := l_script_name_quasto_upgrade;
         end if;
-    elsif 0 = l_arg and l_count = 0
-      then
-        l_script_name_utplsql := 'null.sql';    
-    elsif 0 = l_arg and l_count > 0
-      then
-        if l_current_version != '1.0'
-          then
-            l_script_name_quasto  := l_script_name_quasto_upgrade;
-        end if;
-    elsif l_arg is null
+    end if;
+
+    if l_arg is null
       then
         l_script_name_utplsql  := 'null.sql';
         l_script_name_quasto   := 'null.sql';
         :flag                  := 'N';
         return;
     end if;
+
 
     :script_ut_plsql := l_script_name_utplsql;
     :script_quasto   := l_script_name_quasto;
