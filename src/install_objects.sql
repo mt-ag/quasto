e678fab9ef1b32d975418d180cd7b1d5210b8e22
PhilippDahlem <philipp.dahlem@mt-ag.com>
2024-03-04 17:17:10 +0100

    Revert "Modified install script, updated README.md"
    This reverts commit 764b4a91262fb9372c768fe2001c188798053eb4.

--- src/install_objects.sql
+++ src/install_objects.sql
@@ -3,53 +3,51 @@ set define '~'
 set concat on
 set concat .
 set verify off
-set serveroutput on
 
-
--- QUASTO upgrade version
-variable version varchar2 (50 char)
-exec :version := '23.2';
-
--- script for utPLSQL Objects
 COLUMN :script_ut_plsql NEW_VALUE script_name_utplsql NOPRINT
 variable script_ut_plsql VARCHAR2(50)
 
--- script for QUASTO Objects
+-- Skript for QUASTO Objects
 COLUMN :script_quasto NEW_VALUE script_name_quasto NOPRINT
 variable script_quasto VARCHAR2(50)
 
--- current version installed
+-- current Version installed
 COLUMN :version_old NEW_VALUE script_version_old NOPRINT
 variable version_old VARCHAR2(50)
 
+--Begin Instruction
+PROMPT '------------------------------------'
+PROMPT the install scripts expects 4 diffrent arguments!
+PROMPT these arguments are expected in a certain order
+PROMPT 
+PROMPT example Usage:
+PROMPT @install [1/0] [1/0] [1/0] [1/0]
+PROMPT 
+PROMPT Argument 1: 
+PROMPT Do you have  already ut_plsql installed?
+PROMPT (yes/no) (1/0)
+PROMPT '------------------------------------'
+PROMPT Argument 2:
+PROMPT Do you have already apex installed?
+PROMPT (yes/no) (1/0)
+PROMPT '------------------------------------'
+PROMPT Argument 3:
+PROMPT Do you pass specific Jenkins parameters to the script? 
+PROMPT (yes/no) (1/0)
+PROMPT '------------------------------------'
+PROMPT Argument 4:
+PROMPT Do you already have logger installed?
+PROMPT (yes/no) (1/0)
+PROMPT '------------------------------------'
 
--- begin instruction
-PROMPT '-------------------------------------------------'
-PROMPT The install scripts expects 4 different arguments.
-PROMPT Accepted values: Yes = 1 | No = 0
-PROMPT '-------------------------------------------------'
+SET SERVEROUTPUT ON
 
+variable flag char
+exec :flag := 'Y';
 
--- user input param verifications
-ACCEPT argument1_in CHAR PROMPT 'Do you have installed utPLSQL? (yes/no) (1/0)'
-ACCEPT argument2_in CHAR PROMPT 'Do you have installed Oracle APEX? (yes/no) (1/0)'
-ACCEPT argument3_in CHAR PROMPT 'Do you pass specific Jenkins parameters to the script? (yes/no) (1/0)'
-ACCEPT argument4_in CHAR PROMPT 'Do you already have logger installed? (yes/no) (1/0)'
-
-WHENEVER SQLERROR EXIT FAILURE
-begin
-  if   '~argument1_in' is null or '~argument1_in' not in ('0','1')
-    or '~argument2_in' is null or '~argument2_in' not in ('0','1')
-    or '~argument3_in' is null or '~argument3_in' not in ('0','1')
-    or '~argument4_in' is null or '~argument4_in' not in ('0','1')
-  then
-    RAISE_APPLICATION_ERROR(-20001, 'ERROR: Wrong number of arguments given or invalid values provided.');
-  else
-    dbms_output.put_line('INFO: Continue.');
-  end if;
-end;
-/
-WHENEVER SQLERROR CONTINUE
+-- upgrade version
+variable version varchar2 (50 char)
+exec :version := '23.2';
 
 -- Block to proceess first Argument
 declare
@@ -61,6 +59,7 @@ declare
     l_count_utplsql               number;
     l_version_old                 varchar2 (40 char);
     l_current_version             varchar2 (40 char) := :version;
+    l_arg                         number := '~1';
 
     function get_version_constant return varchar2 as
       l_ret varchar2 (50 char);
@@ -105,10 +104,10 @@ begin
     from user_objects
     where object_name = 'CREATE_UT_TEST_PACKAGES_PKG';
     
-    if '1' = '~argument1_in' and l_count_utplsql > 0
+    if 1 = l_arg and l_count_utplsql > 0
       then 
         l_script_name_utplsql := l_script_name_utplsql_upgrade;
-    elsif '0' = '~argument1_in'
+    elsif 0 = l_arg 
       then
         l_script_name_utplsql := 'null.sql'; 
     end if;
@@ -121,10 +120,11 @@ begin
         end if;
     end if;
 
-    if '~argument1_in' is null
+    if l_arg is null
       then
         l_script_name_utplsql  := 'null.sql';
         l_script_name_quasto   := 'null.sql';
+        :flag                  := 'N';
         return;
     end if;
 
@@ -141,11 +141,15 @@ set feedback off
 set head off
 select :script_ut_plsql from dual;
 select :script_quasto from dual;
+-- DML only does anything if flag stayed Y
+select sysdate from dual
+where :flag = 'Y';
+select 'Wrong Argument or no Argument has been passed correctly!' from dual where :flag != 'Y';
 set feedback on
 set head on
 
 -- Constant Package Generation with last argument as current Version
-@@src/scripts/install_constant_package '~argument1_in' '~argument2_in' '~argument3_in' '~argument4_in' '23.2'
+@@src/scripts/install_constant_package '~1' '~2' '~3' '~4' '23.2'
 
 @@src/~script_name_quasto
 @@src/~script_name_utplsql
