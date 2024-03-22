ccaff7c08ef7c93d5bf81ced547b07246eb760a5
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2024-03-22 18:01:54 +0100

    Moved PL/SQL code with query for Dashboard page to QA_APEX_PKG

--- src/plsql/pkg/qa_apex_pkg.sql
+++ src/plsql/pkg/qa_apex_pkg.sql
@@ -2,13 +2,19 @@ create or replace package qa_apex_pkg is
 
 /******************************************************************************
    NAME:       qa_apex_pkg
-   PURPOSE:    Methods for executing functions used on APEX pages
+   PURPOSE:    Methods for helpers and functionalities used on APEX pages
 
    REVISIONS:
    Release    Date        Author           Description
    ---------  ----------  ---------------  ------------------------------------
    24.1       22.03.2024  mwilhelm         Package has been added to QUASTO
 ******************************************************************************/
+
+  function get_faceted_search_dashboard_data
+  (
+    p_page_id          in number
+   ,p_region_static_id in varchar2
+  ) return test_results_table_t pipelined;
 
   /**
    * procedure to upload a json rule file
@@ -37,6 +43,64 @@ create or replace package qa_apex_pkg is
 end qa_apex_pkg;
 /
 create or replace package body qa_apex_pkg as
+
+  function get_faceted_search_dashboard_data
+  (
+    p_page_id          in number
+   ,p_region_static_id in varchar2
+  ) return test_results_table_t pipelined
+  is
+    l_region_id number;
+    l_context   apex_exec.t_context;
+
+    type t_col_index is table of pls_integer index by varchar2(255);
+    l_col_index t_col_index;
+
+    procedure get_column_indexes (pi_columns in varchar2_tab_t) 
+    is 
+    begin
+      for i in 1..pi_columns.count loop
+        l_col_index (pi_columns(i)) := apex_exec.get_column_position(p_context     => l_context
+                                                                    ,p_column_name => pi_columns(i));
+      end loop;
+    end get_column_indexes;
+  begin
+    select region_id
+      into l_region_id
+      from apex_application_page_regions
+     where application_id = V('APP_ID')
+       and page_id = p_page_id
+       and static_id = p_region_static_id;
+
+    l_context := apex_region.open_query_context(p_page_id   => p_page_id
+                                               ,p_region_id => l_region_id);
+
+    get_column_indexes(pi_columns => varchar2_tab_t('QATR_ID','QATR_DATE', 'QATR_SCHEME_NAME', 'QARU_CATEGORY', 'QATR_RESULT', 'QARU_NAME', 'QARU_LAYER', 'QARU_ERROR_LEVEL', 'QARU_IS_ACTIVE', 'QARU_CLIENT_NAME', 'QATR_PROGRAM_NAME'));
+
+    while apex_exec.next_row(p_context => l_context) loop
+      pipe row(test_results_row_t(
+                    apex_exec.get_number  (p_context => l_context, p_column_idx => l_col_index('QATR_ID')),
+                    apex_exec.get_date    (p_context => l_context, p_column_idx => l_col_index('QATR_DATE')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QATR_SCHEME_NAME')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_CATEGORY')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QATR_RESULT')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_NAME')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_LAYER')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_ERROR_LEVEL')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_IS_ACTIVE')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QARU_CLIENT_NAME')),
+                    apex_exec.get_varchar2(p_context => l_context, p_column_idx => l_col_index('QATR_PROGRAM_NAME'))));
+    end loop;
+
+    apex_exec.close(l_context);
+    return;
+  exception
+    when no_data_needed then
+      apex_exec.close(l_context);
+    when others then
+      apex_exec.close(l_context);
+      raise;
+  end get_faceted_search_dashboard_data;
 
   procedure p_upload_rules_json(
     pi_file_name in varchar2
