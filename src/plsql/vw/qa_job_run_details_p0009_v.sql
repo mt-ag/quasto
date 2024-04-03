76e2e5838b4e2ca9c0dc848372824808a42071d2
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2024-04-03 19:06:21 +0200

    Changed format of job runtime duration

--- src/plsql/vw/qa_job_run_details_p0009_v.sql
+++ src/plsql/vw/qa_job_run_details_p0009_v.sql
@@ -1,7 +1,7 @@
 
   CREATE OR REPLACE FORCE EDITIONABLE VIEW "QA_JOB_RUN_DETAILS_P0009_V"
   AS 
-select job_name, log_date, status, error#, errors, run_duration
+select job_name, log_date, status, error#, errors, lpad(extract(hour from run_duration),2,'0') || ':' || lpad(extract(minute from run_duration),2,'0') || ':' || lpad(extract(second from run_duration),2,'0') as run_duration
 from USER_SCHEDULER_JOB_RUN_DETAILS
 order by log_date desc
 ;
