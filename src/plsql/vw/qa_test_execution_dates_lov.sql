840ccb163bb9cd738b15a5af47944847d8b8e877
Maurice Wilhelm <maurice.wilhelm@mt-ag.com>
2024-04-22 21:08:09 +0200

    #QUASTO-53.Fix2: Removed trunc from execution dates LOV

--- src/plsql/vw/qa_test_execution_dates_lov.sql
+++ src/plsql/vw/qa_test_execution_dates_lov.sql
@@ -3,9 +3,9 @@
   AS 
 select to_char(v.qatr_date, 'MM/DD/YYYY') as display_value
      , to_char(v.qatr_date, 'fmMM/DD/YYYY') as return_value
-  from (select trunc(qatr_date) as qatr_date
+  from (select qatr_date
         from QA_OVERVIEW_TESTS_P0001_V
-        group by trunc(qatr_date)) v
+        group by qatr_date) v
 order by v.qatr_date desc
 ;
 /
