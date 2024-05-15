69b6c90b92a8d4957cbe03fb07b0926d2f2b38e1
PhilippDahlem <philipp.dahlem@mt-ag.com>
2024-05-15 13:49:48 +0200

    2.24 - QUASTO-91 Error statt Failed anzeigen

--- src/plsql/vw/qa_job_run_details_p0011_v.sql
+++ src/plsql/vw/qa_job_run_details_p0011_v.sql
@@ -1,9 +1,16 @@
-
-  CREATE OR REPLACE FORCE EDITIONABLE VIEW "QA_JOB_RUN_DETAILS_P0011_V"
-  AS 
-select job_name, log_date, status, error#, errors, lpad(to_char(extract(hour from run_duration)),2,'0') || ':' || lpad(to_char(extract(minute from run_duration)),2,'0') || ':' || lpad(to_char(extract(second from run_duration)),2,'0') as run_duration
-from USER_SCHEDULER_JOB_RUN_DETAILS
+create or replace force view qa_job_run_details_p0011_v as
+select job_name
+      ,log_date
+      ,case when status = 'FAILED' then 'ERROR' else status end as status
+      ,error#
+      ,errors
+      ,lpad(to_char(extract(hour from run_duration))
+           ,2
+           ,'0') || ':' || lpad(to_char(extract(minute from run_duration))
+                               ,2
+                               ,'0') || ':' || lpad(to_char(extract(second from run_duration))
+                                                   ,2
+                                                   ,'0') as run_duration
+from user_scheduler_job_run_details
 where job_name != qa_utils_pkg.f_get_constant_string_value('gc_utplsql_scheduler_cronjob_name')
-order by log_date desc
-;
-/
+order by log_date desc;
