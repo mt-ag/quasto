62cfc322b26b7609cb2d6881f305d59bee933d6d
PhilippDahlem <philipp.dahlem@mt-ag.com>
2024-05-15 14:40:33 +0200

    2.24 - QUASTO-92

--- src/plsql/vw/qa_job_details_P0009_v.sql
+++ src/plsql/vw/qa_job_details_P0009_v.sql
@@ -1,8 +1,16 @@
-
-  CREATE OR REPLACE FORCE EDITIONABLE VIEW "QA_JOB_DETAILS_P0009_V"
-  AS 
-select START_DATE, LAST_START_DATE, lpad(to_char(extract(hour from LAST_RUN_DURATION)),2,'0') || ':' || lpad(to_char(extract(minute from LAST_RUN_DURATION)),2,'0') || ':' || lpad(to_char(extract(second from LAST_RUN_DURATION)),2,'0') as LAST_RUN_DURATION, NEXT_RUN_DATE, REPEAT_INTERVAL, STATE
-from USER_SCHEDULER_JOBS
+create or replace force view qa_job_run_details_p0009_v as
+select job_name
+      ,log_date
+      ,case when status = 'FAILED' then 'ERROR' else status end as status
+      ,error#
+      ,errors
+      ,lpad(to_char(extract(hour from run_duration))
+           ,2
+           ,'0') || ':' || lpad(to_char(extract(minute from run_duration))
+                               ,2
+                               ,'0') || ':' || lpad(to_char(extract(second from run_duration))
+                                                   ,2
+                                                   ,'0') as run_duration
+from user_scheduler_job_run_details
 where job_name = qa_utils_pkg.f_get_constant_string_value('gc_utplsql_scheduler_cronjob_name')
-;
-/
+order by log_date desc;
