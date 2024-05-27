6ed775d9f6aca1f3ed4846328506b9479a61810e
PhilippDahlem <philipp.dahlem@mt-ag.com>
2024-05-27 23:01:22 +0200

    2.24 -  QUASTO-97

--- src/plsql/pkg/qa_apex_api_pkg.sql
+++ src/plsql/pkg/qa_apex_api_pkg.sql
@@ -93,7 +93,7 @@ create or replace package body qa_apex_api_pkg as
                             ,p_params => l_param_list);
       raise;
   end p_exclude_not_whitelisted_apex_entries;
-  
+
 
   function tf_run_rule
   (
@@ -108,8 +108,8 @@ create or replace package body qa_apex_api_pkg as
     l_param_list qa_logger_pkg.tab_param;
   
     l_rule_active boolean;
-    l_qa_rule       qa_rule_t;
-    l_qa_rules      qa_rules_t := new qa_rules_t();
+    l_qa_rule     qa_rule_t;
+    l_qa_rules    qa_rules_t := new qa_rules_t();
   begin
     qa_logger_pkg.append_param(p_params  => l_param_list
                               ,p_name_01 => 'pi_qaru_rule_number'
@@ -125,18 +125,20 @@ create or replace package body qa_apex_api_pkg as
       raise_application_error(-20001
                              ,'Missing input parameter value for pi_qaru_rule_number: ' || pi_qaru_rule_number || ' or pi_qaru_client_name: ' || pi_qaru_client_name || ' or pi_target_scheme: ' || pi_target_scheme);
     end if;
-    
-      l_rule_active := qa_main_pkg.f_is_rule_active(pi_qaru_rule_number => pi_qaru_rule_number
-                                                   ,pi_qaru_client_name => pi_qaru_client_name);
-      if l_rule_active = false
-      then
-        raise_application_error(-20001
-                               ,'Rule is not set to active for rule number: ' || pi_qaru_rule_number || ' and client name: ' || pi_qaru_client_name);
-      end if;
-    
-      l_qa_rule := qa_main_pkg.f_get_rule(pi_qaru_rule_number => pi_qaru_rule_number
-                                         ,pi_qaru_client_name => pi_qaru_client_name);
-    
+  
+    l_rule_active := qa_main_pkg.f_is_rule_active(pi_qaru_rule_number => pi_qaru_rule_number
+                                                 ,pi_qaru_client_name => pi_qaru_client_name);
+    if l_rule_active = false
+    then
+      raise_application_error(-20001
+                             ,'Rule is not set to active for rule number: ' || pi_qaru_rule_number || ' and client name: ' || pi_qaru_client_name);
+    end if;
+  
+    l_qa_rule := qa_main_pkg.f_get_rule(pi_qaru_rule_number => pi_qaru_rule_number
+                                       ,pi_qaru_client_name => pi_qaru_client_name);
+  
+    if l_qa_rule.qaru_category = 'APEX'
+    then
       execute immediate l_qa_rule.qaru_sql bulk collect
         into l_qa_rules
       -- :1 scheme
@@ -151,7 +153,19 @@ create or replace package body qa_apex_api_pkg as
         using pi_target_scheme, l_qa_rule.qaru_id, l_qa_rule.qaru_category, l_qa_rule.qaru_error_level, l_qa_rule.qaru_object_types, l_qa_rule.qaru_error_message, l_qa_rule.qaru_sql, pi_app_id, pi_page_id;
       --Remove entries that dont belong to the current owner
       qa_apex_api_pkg.p_exclude_not_whitelisted_apex_entries(pi_qa_rules_t => l_qa_rules);
-      return l_qa_rules;
+    else
+      execute immediate l_qa_rule.qaru_sql bulk collect
+        into l_qa_rules
+      -- :1 scheme
+      -- :2 qaru_id
+      -- :3 qaru_category
+      -- :4 qaru_error_level
+      -- :5 qaru_object_types
+      -- :6 qaru_error_message    
+      -- :7 qaru_sql
+        using pi_target_scheme, l_qa_rule.qaru_id, l_qa_rule.qaru_category, l_qa_rule.qaru_error_level, l_qa_rule.qaru_object_types, l_qa_rule.qaru_error_message, l_qa_rule.qaru_sql;
+    end if;
+    return l_qa_rules;
   exception
     when no_data_found then
       qa_logger_pkg.p_qa_log(p_text   => 'No Data found while selecting from qa_rules'
